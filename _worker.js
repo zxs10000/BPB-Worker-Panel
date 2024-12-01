0df7e0c9385c57aec31f42450f248a9b9f9c880e
zxs10000 <118518227+zxs10000@users.noreply.github.com>
2024-12-01 13:28:16 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ js-sha256/src/sha256.js:
    * [js-sha256]{@link https://github.com/emn178/js-sha256}
    *
    * @version 0.11.0
-   * @author Chen, Yi-Cyuan [emn178@gmail.com]
+   * @author Chen, Yi-Cyuan [emn178@gmail.c]
    * @copyright Chen, Yi-Cyuan 2014-2024
    * @license MIT
    *)
